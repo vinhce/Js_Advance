37abcd3b3252155dd83425a9302f2702a9753f33
vinhce <thohight@gmail.com>
2026-09-17 15:02:41 +0700

    Create js.js

---
+++ js.js
@@ -0,0 +1,11 @@
+// Declare and assign values to variables
+let name = "Nguyen Van An";
+let age = 20;
+let address = "Hanoi";
+let isStudent = true;
+
+// Print the variables to the console in the specified format
+console.log("Full Name: " + name);
+console.log("Age: " + age);
+console.log("Address: " + address);
+console.log("Is Student: " + isStudent);
